b5fae017cea5456f498d0ae9ac2938c81620d2e8
poulpi <pdepoulpiquet@mediarithmics.com>
2017-09-29 17:11:59 +0200

    Fix indentation

--- examples/simple-ad-renderer/src/MyPluginImpl.ts
+++ examples/simple-ad-renderer/src/MyPluginImpl.ts
@@ -1,29 +1,29 @@
 import { core } from "@mediarithmics/plugins-nodejs-sdk";
 
 export class MySimpleAdRenderer extends core.AdRendererBasePlugin<
-core.AdRendererBaseInstanceContext
+  core.AdRendererBaseInstanceContext
 > {
-protected async onAdContents(
-  request: core.AdRendererRequest,
-  instanceContext: core.AdRendererBaseInstanceContext
-): Promise<core.AdRendererPluginResponse> {
-  const result: core.AdRendererPluginResponse = {
-    html: `<html>
+  protected async onAdContents(
+    request: core.AdRendererRequest,
+    instanceContext: core.AdRendererBaseInstanceContext
+  ): Promise<core.AdRendererPluginResponse> {
+    const result: core.AdRendererPluginResponse = {
+      html: `<html>
     <body>
     <h1>Creative: ${instanceContext.creative.name}</h1>
     <br/>
     <p>
     Powered by the Ad Renderer: ${instanceContext.creative
       .renderer_group_id}:${instanceContext.creative
-      .renderer_artifact_id} v.${instanceContext.creative
-      .renderer_version_value}
+        .renderer_artifact_id} v.${instanceContext.creative
+        .renderer_version_value}
     </p>
     <!-- We always need to include the mediarithmics impression tracking pixel -->
     <img src="${request.display_tracking_url}" />
     </body>
     </html>`
-  };
+    };
 
-  return Promise.resolve(result);
-}
+    return Promise.resolve(result);
+  }
 }
