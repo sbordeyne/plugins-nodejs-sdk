a78a30c50fa1b5b7fdebaee51d657e6e303c9d92
poulpi <pdepoulpiquet@mediarithmics.com>
2017-11-23 17:26:32 +0100

    Fix the JSON vs non JSON situations

--- src/core/class/mediarithmics/common/BasePlugin.ts
+++ src/core/class/mediarithmics/common/BasePlugin.ts
@@ -168,8 +168,10 @@ export abstract class BasePlugin {
       return await this._transport(options);
     } catch (e) {
       if (e.name === "StatusCodeError") {
+        const bodyString =
+          isJson !== undefined && !isJson ? body : JSON.stringify(body);
         throw new Error(
-          `Error while calling ${method} '${uri}' with the request body '${JSON.stringify(body) ||
+          `Error while calling ${method} '${uri}' with the request body '${bodyString ||
             ""}': got a ${e.response.statusCode} ${e.response
             .statusMessage} with the response body ${JSON.stringify(
             e.response.body
