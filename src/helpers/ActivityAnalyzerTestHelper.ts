8eb66732d5e1b1fb3db087aa2730e302e37328dc
Alexandre Annic <aannic.ext@mediarithmics.com>
2019-03-15 11:18:52 +0100

    Clean

--- src/helpers/ActivityAnalyzerTestHelper.ts
+++ src/helpers/ActivityAnalyzerTestHelper.ts
@@ -23,9 +23,9 @@ const activityAnalyzer: core.ActivityAnalyzerResponse = {
 
 const rpMockupGlobal: sinon.SinonStub = sinon.stub();
 
-const mockApi = (urlPatterrn: RegExp): SinonStub => {
+const mockApi = (uriPattern: RegExp): SinonStub => {
     return rpMockupGlobal
-        .withArgs(sinon.match.has('uri', sinon.match((value: string) => value.match(urlPatterrn) !== null)))
+        .withArgs(sinon.match.has('uri', sinon.match((value: string) => value.match(uriPattern) !== null)))
 };
 
 mockApi(/\/v1\/activity_analyzers\/(.){1,10}/).returns(activityAnalyzer);
@@ -39,7 +39,6 @@ const itFactory = (
         const rpPostStub = sinon.stub(rp, 'post').returns({});
         mockApi(/\/v1\/activity_analyzers\/(.){1,10}\/properties/).returns(property);
 
-        // All the magic is here
         const runner = new core.TestingPluginRunner(plugin, rpMockupGlobal);
 
         request(runner.plugin.app)
